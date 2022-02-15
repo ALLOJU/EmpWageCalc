b5cb93a3faa0eea5dce0aed112a4036fd3259a9d
MounikaAlloju <mounika.alloju@gmail.com>
2022-02-15 13:54:37 +0530

    Master branch

---
+++ src/com/bridgelabz/Main.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class Main {
+	
+public static void main(String[] args) {
+	
+	//constants
+	
+
+
+	
+	System.out.println("Welcome to Employee Wage Calculation");
+	
+	
+}
+	
+	
+}
+
